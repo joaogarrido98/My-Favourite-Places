348a0edf425b9a650b386aabb73c501c7517efa5
joaogarrido98 <joao.melo.garrido@gmail.com>
2021-11-18 17:56:21 +0000

    Initial setup of Storyboard and Swift source file for the places view controller

--- My Favourite Places/PlacesViewController.swift
+++ My Favourite Places/PlacesViewController.swift
@@ -11,7 +11,6 @@ class PlacesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
