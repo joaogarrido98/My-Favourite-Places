a1787beacf9743ad130bbdd6a68fcc84994a86ca
joaogarrido98 <joao.melo.garrido@gmail.com>
2021-11-18 17:54:36 +0000

    Initial Commit

---
+++ My Favourite Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Favourite Places
+//
+//  Created by Ilkin Samedzade on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
